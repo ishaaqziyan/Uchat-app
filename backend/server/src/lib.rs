c2667d68ae33eccec42b8b7c63fdc9ef9c89d239
ishaaq <ishaaqziyan@gmail.com>
2024-02-03 14:25:54 +0530

    Implemented the Database Connection Extractor

--- backend/server/src/lib.rs
+++ backend/server/src/lib.rs
@@ -4,6 +4,7 @@ use uchat_query::{AsyncConnection, AsyncConnectionPool, QueryError};
 pub mod logging;
 pub mod router;
 pub mod error;
+pub mod extractor;
 
 #[derive(FromRef, Clone)]
 pub struct AppState {
