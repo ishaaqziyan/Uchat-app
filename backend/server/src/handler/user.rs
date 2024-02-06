5779d8ed6e4a23136ef423ad7032a4537cd4646f
ishaaq <ishaaqziyan@gmail.com>
2024-02-06 10:03:08 +0530

    Refactored code with macro_rules! and Implemented DB IDs

--- backend/server/src/handler/user.rs
+++ backend/server/src/handler/user.rs
@@ -1,7 +1,7 @@
 use axum::{async_trait, Json};
 use hyper::StatusCode;
 use tracing::info;
-use uchat_endpoint::user::endpoint::{CreateUser, CreateUserOk};
+use uchat_endpoint::user::endpoint::{CreateUser, CreateUserOk, Login, LoginOk};
 
 use crate::{error::ApiResult, extractor::DbConnection, AppState};
 
@@ -27,5 +27,17 @@ impl PublicApiRequest for CreateUser {
                 username: self.username,
             }),
         ))
+    }
+}
+
+#[async_trait]
+impl PublicApiRequest for Login {
+    type Response = (StatusCode, Json<LoginOk>);
+    async fn process_request(
+        self,
+        DbConnection(mut conn): DbConnection,
+        state: AppState,
+    ) -> ApiResult<Self::Response> {
+        todo!();
     }
 }
--- frontend/src/page.rs
+++ frontend/src/page.rs
@@ -1,9 +1,13 @@
 pub mod register;
+pub mod login;
 
+pub use login::Login;
 pub use register::Register;
+
 
 pub use route::*;
 
 pub mod route {
+    pub const ACCOUNT_LOGIN : &str = "/account/login";
     pub const ACCOUNT_REGISTER: &str = "/account/register";
 }
