60e896293a0e5934fe9858cd5cf1f33f5485e9f9
ishaaq <ishaaqziyan@gmail.com>
2024-02-10 12:07:07 +0530

    Implemented the Navigation Bar and added Navigation Buttons

---
+++ frontend/src/elements/navbar.rs
@@ -0,0 +1,65 @@
+#![allow(non_snake_case)]
+
+use crate::prelude::*;
+use dioxus::prelude::*;
+
+#[derive(Props)]
+pub struct NavButtonProps<'a> {
+    img: & 'a str,
+    label: & 'a str,
+    onclick:EventHandler<'a, MouseEvent>,
+    highlight: Option<bool>,
+    children: Element<'a>,
+}
+
+pub fn NavButton <'a> (cx: Scope <'a, NavButtonProps<'a>>) -> Element {
+
+    let selected_bgcolor =maybe_class!("bg-slate-500",
+         matches!(cx.props.highlight, Some(true))
+    );
+    cx.render(rsx! {
+           button { 
+            class: "cursor-pointer flex flex-col items-center justify-center h-full {selected_bgcolor}",
+            onclick: move |ev| cx.props.onclick.call(ev),
+            img {
+                class: "invert",
+                src: cx.props.img,
+                width: "25px",
+                height: "25px"
+            },
+            div {
+                class: "text-sm text-white",
+                cx.props.label
+            },
+            &cx.props.children
+           }
+    })
+}
+
+pub fn Navbar(cx: Scope) -> Element {
+    cx.render(rsx! {
+        nav {
+        class: "max-w-[var(--content-max-width) h-[var(--navbar-height)]
+        fixed bottom-0 left-0 right-0 mx-auto
+        border-t navbar-bg-color navbar-border-color",
+        div {
+            class: "grid grid-cols-3 justify-around w-full h-full items-center shadow-inner",
+            NavButton {
+                img: "/static/icons/icon-home.svg",
+                label: "Home",
+                onclick: |_| (),
+            },
+            NavButton {
+                img: "/static/icons/icon-trending.svg",
+                label: "Trending",
+                onclick: |_| (),
+            },
+            NavButton {
+                img: "/static/icons/icon-new-post.svg",
+                label: "Post",
+                onclick: |_| (),
+            }
+        }
+    }
+    })
+}
