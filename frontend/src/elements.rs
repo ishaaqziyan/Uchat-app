1226c0d6129c8c902dd87de4bf6d1d35da3f85b7
ishaaq <ishaaqziyan@gmail.com>
2024-03-04 10:09:39 +0530

    Implemented the App Bar Layout

--- frontend/src/elements.rs
+++ frontend/src/elements.rs
@@ -2,6 +2,8 @@ pub mod keyed_notification_box;
 pub mod navbar;
 pub mod post;
 pub mod toaster;
+pub mod appbar;
 
 pub use keyed_notification_box::KeyedNotificationBox;
 pub use navbar::Navbar;
+pub use appbar::Appbar;
